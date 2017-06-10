f4819dcb87e170bf25237bfd3a1c6a48725432d9
Alex Cudriz <acudriz@tie-in.co>
2017-06-10 08:29:28 -0500

    video#8

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const Product = require('./models/product')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -18,8 +21,21 @@ app.get('/api/product/:productId', (req, res) => {
 })
 
 app.post('/api/product', (req, res) => {
-  console.log(req.body)
-  res.status(200).send({message: "El producto se ha recibido"})
+  console.log('POST /api/product');
+  console.log(req.body);
+
+  let product = new Product()
+  product.name = req.body.name
+  product.picture = req.body.picture
+  product.price = req.body.price
+  product.category = req.body.category
+  product.description = req.body.description
+
+  product.save((err, productStored) => {
+    if (err) res.status(500).send({message: `Error al salvar en la base de datos ${err}`})
+
+    res.status(200).send({product: productStored})
+  })
 })
 
 app.put('/api/product/:productId', (req, res) => {
@@ -30,6 +46,11 @@ app.delete('/api/product/:productId', (req, res) => {
 
 })
 
-app.listen(port, () => {
-  console.log(`API REST corriendo en http://localhost:${port}`);
+mongoose.connect('mongodb://admin:admin@ds119302.mlab.com:19302/pruebas', (err, res) => {
+  if (err) throw err
+  console.log('conexion a la base de datos establecida...');
+
+  app.listen(port, () => {
+    console.log(`API REST corriendo en http://localhost:${port}`);
+  })
 })
